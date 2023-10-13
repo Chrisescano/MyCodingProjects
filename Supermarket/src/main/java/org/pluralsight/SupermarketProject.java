4d3bf71564953ec82d2fa943ae91f2aea0c7198f
Christian Escano <escanochristian21@gmail.com>
2023-10-12 22:16:02 -0400

    Added: Supermarket items, prices and shopping cart

--- Supermarket/src/main/java/org/pluralsight/SupermarketProject.java
+++ Supermarket/src/main/java/org/pluralsight/SupermarketProject.java
@@ -8,6 +8,21 @@ public class SupermarketProject {
     private static String welcomePrompt = "Welcome to Greg's supermarket!";
     private static String mainMenuPrompt = "Possible commands:\n  (B)rowse items\n  (P)rint receipt\n  (Q)uit\nType command: ";
 
+    static String[] listOfProducts = {
+            "Apple", "Banana", "Coconut", "Doughnuts", "Eggs", "Fig", "Granola",
+            "Hash-Browns", "Ice", "Jam", "Kiwi", "Lychee", "Milk", "Nuggets",
+            "Oxtail", "Pears", "Quinoa", "Rice", "Steak", "Tomatoes", "Udon-Noodles",
+            "Velveeta-Cheese", "Water-Bottle", "Xanthan-Gum", "Yogurt", "Zebra-Cake"
+    };
+    static double[] priceOfProducts = {
+            1.00, 1.17, 3.43, 5.97, 6.72, 6.05, 1.89,
+            3.25, 0.99, 2.48, 2.98,12.45, 3.14, 9.15,
+            15.89,2.20, 4.70,13.99,10.20, 1.50, 2.49,
+            6.19, 1.50,13.00, 2.36, 2.79
+    };
+    static String[] shoppingCart = new String[30];
+    static int shoppingCartIndex = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
